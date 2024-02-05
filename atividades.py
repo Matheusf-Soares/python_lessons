284b4e3c9db4a1305f1a742559cf807b3d8e46ec
Matheusf-Soares <matheusfps27@icloud.com>
2024-02-04 22:06:46 -0300

    Capitulo 3 completo

--- atividades.py
+++ atividades.py
@@ -68,11 +68,325 @@ def cumprimentos_32():
     for nome in nomes:
         print(mensagem.format(nome.title()))
 
-
 def propria_lista_33():
     carros = ['Koenigsegg Agera RS', 'Lamborghini Gallardo', 'Aston Martin Valkyrie']
     mensagem = 'Eu adoraria ter um {}.'
     for carro in carros:
         print(mensagem.format(carro))
 
-propria_lista_33()
+def lista_convidados_34():
+    convidados = [
+        'albert einstein',
+        'stephen hawking',
+        'eduardo saverin',
+        'jeff bezos'
+    ]
+    mensagem = 'Adoraria que comparecesse em meu jantar, {}.'
+
+    for convidado in convidados:
+        print(mensagem.format(convidado))
+    
+    return convidados, mensagem
+
+import random
+def mudando_lista_convidados_35():
+    convidados, mensagem = lista_convidados_34()
+    indice_faltante = (random.randint(0, len(convidados) - 1))
+    print(f'{convidados[indice_faltante].title()} não comparecerá ao jantar.')
+    convidados[indice_faltante] = 'mark zuckerberg'
+    print(mensagem.format(convidados[indice_faltante].title()))
+
+def mais_convidados_36():
+    convidados, mensagem = lista_convidados_34()
+    print('Foi encontrada uma mesa maior.')
+    convidados.insert(0, 'mark zuckerberg')
+    convidados.insert(len(convidados) // 2, 'elon musk')
+    convidados.append('kunimitsu takahashi')
+    for convidado in convidados:
+        print(mensagem.format(convidado.title()))
+    
+    return convidados, mensagem
+
+def reduzindo_lista_convidados_37():
+    convidados, mensagem = mais_convidados_36()
+    print('Apenas duas pessoas poderão ser convidadas para o jantar. :(')
+
+    while len(convidados) > 2:
+        print(f'Desculpe, {convidados.pop().title()}, mas o jantar foi cancelado. :(')
+
+    for convidado in convidados:
+        print(f'A festa continua. Espero sua presença, {convidado.title()}.')
+
+def conhecendo_mundo_38():
+    lugares = ['monte everest', 'andes', 'caribe', 'cancun', 'fernando de noronha']
+    print(' - '.join(x.title() for x in lugares))
+    print(' - '.join(sorted(x.title() for x in lugares)))
+    lugares.reverse()
+    print(' - '.join(x.title() for x in lugares))
+    lugares.reverse()
+    print(' - '.join(x.title() for x in lugares))
+    lugares.sort()
+    print(' - '.join(x.title() for x in lugares))
+    lugares.sort(reverse=True)
+    print(' - '.join(x.title() for x in lugares))   
+
+def convidados_jantar_39():
+    convidados, mensagem  = lista_convidados_34()
+    print(f'{len(convidados)} pessoas estão sendo convidados para o jantar.')
+
+def funcoes_310():
+    armas = [
+        'Accuracy International AS50',
+        'Ag m/42',
+        'AGM-1 Carbine',
+        'AK-74',
+        'AK74U',
+        'Benelli Argo EL',
+        'Benelli MR1',
+        'Beretta BM59',
+        'Beretta Cx4 Storm',
+        'Bushmaster M4 Type Carbine',
+        'Carbon 15',
+        'Charlton Automatic Rifle',
+        'FN Scar-H SV',
+        'FN Model 1949',
+        'Fusil Automatique Modèle 1917',
+        'General Liu rifle',
+        'German Sport Guns GSG-5',
+        'Gewehr 41',
+        'Heckler & Koch HK41',
+        'Heckler & Koch HK43',
+        'Heckler & Koch PSG1',
+        'Heckler & Koch SL7',
+        'Hi-Point Carbine',
+        'Howa Type 64',
+        'Howard Francis machine carbine',
+        'Howell Automatic Rifle',
+        'Kbsp wz. 1938M',
+        'Kintrek KBP-1',
+        'L1A1 Self-Loading Rifle',
+        'M110 Semi-Automatic Sniper System',
+        'M4A1',
+        'M4A4',
+        'M89SR sniper rifle',
+        'Norinco JW-20',
+        'MG 42',
+        'MP 40',
+        'Norinco NHM 91',
+        'Olin/Winchester Salvo Rifle',
+        'P90',
+        'PP2000',
+        'Pauza P-50',
+        'Pedersen rifle',
+        'Preetz Model 65',
+        'PSL (rifle)',
+        'PTR 91F',
+        'QBU-88',
+        'Rasheed Carbine',
+        'Remington Model 24',
+        'Remington Model 522 Viper',
+        'Remington Nylon 66',
+        'Remington Semi Automatic Sniper System',
+        'Rieder Automatic Rifle',
+        'Ruger SR-556',
+        'Smith & Wesson M&P10',
+        'Smith & Wesson M&P15',
+        'Smith & Wesson M&P15-22',
+        'Steyr IWS 2000',
+        'UMP45',
+        'Volkssturmgewehr',
+        'Vulcan V18',
+        'Vz. 52 rifle',
+        'Walther G22',
+        'Walther WA 2000',
+        'Winchester model 30',
+        'Zastava M91',
+        'Zijiang M99'
+    ]
+
+    print(f'Total de armas: {len(armas)}')
+    print('  ︻┳═一 \n'.join(armas))
+
+    for _ in range(random.randint(1, len(armas))):
+        print(f'{armas.pop()} foi excluída do estoque.')
+
+    print(f'Total de armas restantes: {len(armas)}')
+
+    arma_a_comprar = input('Qual arma deseja comprar?')
+    guns_number = armas.count(arma_a_comprar)
+    if guns_number > 0:
+        print(f'{arma_a_comprar} foi vendida. ︻┳═一 🛍️')
+        armas.remove(arma_a_comprar)
+    else:
+        print(f'{arma_a_comprar} não está no estoque.')
+
+    print('Lista de armas restantes:')
+    print('  ︻┳═一 \n'.join(reversed(armas)))
+    
+def erro_intencional_311():
+    lista = ['0']
+    index = int(input('Entre com um numero maior que 0 para gerar um erro.'))
+    print(lista[index])
+    
+erro_intencional_311()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
